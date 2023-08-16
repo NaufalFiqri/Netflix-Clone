d640aa7d7d89ffa2ed97a5e26fb8da09c968b21a
NaufalFiqri <naufal11bale@gmail.com>
2023-08-16 16:25:12 +0800

    finish header js

--- assets/js/global.js
+++ assets/js/global.js
@@ -4,3 +4,12 @@
 const addEventOnElements = function (elements, eventType, callback) {
     for (const elem of elements) elem.addEventListener(eventType, callback);
   }
+
+// Toggle search box in mobile device || small screen
+const searchBox = document.querySelector("[search-box]");
+const searchTogglers = document.querySelectorAll("[search-toggler]");
+
+addEventOnElements(searchTogglers, "click", function () {
+  searchBox.classList.toggle("active");
+});
+
