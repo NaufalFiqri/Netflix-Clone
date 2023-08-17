de08da35f532ba20890dc1be1da642fd2153f043
NaufalFiqri <naufal11bale@gmail.com>
2023-08-17 13:17:16 +0800

    updated detail.js

--- assets/js/global.js
+++ assets/js/global.js
@@ -1,9 +1,9 @@
-'use strict';
+"use strict";
 
 // Add event on multiple elements
 const addEventOnElements = function (elements, eventType, callback) {
-    for (const elem of elements) elem.addEventListener(eventType, callback);
-  }
+  for (const elem of elements) elem.addEventListener(eventType, callback);
+};
 
 // Toggle search box in mobile device || small screen
 const searchBox = document.querySelector("[search-box]");
@@ -13,3 +13,12 @@ addEventOnElements(searchTogglers, "click", function () {
   searchBox.classList.toggle("active");
 });
 
+// store movieID in `LocalStorage` when you click any card
+const getMovieDetail = function (movieId) {
+  window.localStorage.setItem("movieId", String(movieId));
+};
+
+const getMovieList = function (urlParam, genreName) {
+  window.localStorage.setItem("urlParam", urlParam);
+  window.localStorage.setItem("genreName", genreName);
+};
